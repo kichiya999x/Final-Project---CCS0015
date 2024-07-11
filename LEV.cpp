47eee7d5cdab8fea4459e68c5a4c5770f7683e9c
kichiya999x <kiesha.conde@gmail.com>
2024-07-12 04:56:48 +0800

    added student and course structures

---
+++ LEV.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct Student {
+    string id;
+    string lastName;
+    string firstName;
+    string middleName;
+    string gender;
+    string birthday;
+    string address;
+    string degreeProgram;
+    int yearLevel;
+};
+
+struct Course {
+    string courseCode;
+    string courseTitle;
+    int units;
+    int yearLevel;
+};
